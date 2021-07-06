e1e0f76d99cdff201fb754944bbfbdb56c976075
Budi <aku@budi.my.id>
2021-07-06 13:27:50 +0700

    Update AttachmentController.php

--- src/Controllers/AttachmentController.php
+++ src/Controllers/AttachmentController.php
@@ -50,16 +50,11 @@ class AttachmentController extends Controller
     public function show($id)
     {
         $attachment = Mail_Attachment::findOrFail($id);
-		//if(env('FILESYSTEM_DRIVER')=="cloudinary")
-		//{
-			$path = "../storage/logs/". $id;
-			file_put_contents($path, file_get_contents($attachment->file_url));
-			return response()->download($path, Str::ascii($attachment->file_name))->deleteFileAfterSend();
-		//}
-		//else
-		//{
-			//return Storage::disk('public')->download($attachment->file_path, Str::ascii($attachment->file_name) );
-		//}
+        
+        print($attachment->file_url);
+        //$path = "../storage/logs/". $id;
+        //file_put_contents($path, file_get_contents($attachment->file_url));
+        //return response()->download($path, Str::ascii($attachment->file_name))->deleteFileAfterSend();
     }
 
     /**
