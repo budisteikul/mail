71735477d673b96647863f10537d783c7e630303
Budi <aku@budi.my.id>
2021-07-06 20:40:22 +0700

    Create menu.blade.php

---
+++ src/views/layouts/menu.blade.php
@@ -0,0 +1,5 @@
+            <li class="nav-item dropdown no-arrow mx-1">
+              <a class="nav-link" target="_blank" href="/mails">
+                <i class="fas fa-envelope fa-fw"></i>
+              </a>     
+            </li>
