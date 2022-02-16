313b4fe0d488fe2712cbbc7303081cf6b61141c3
Budi <aku@budi.my.id>
2022-02-16 09:37:38 +0700

    #1891

--- src/MailServiceProvider.php
+++ src/MailServiceProvider.php
@@ -24,10 +24,11 @@ class MailServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__.'/views', 'mail');
-        $this->loadMigrationsFrom(__DIR__.'/migrations/2019_01_11_142447_create_mail_accounts_table.php');
-        $this->loadMigrationsFrom(__DIR__.'/migrations/2019_01_12_134154_create_mail_options_table.php');
-        $this->loadMigrationsFrom(__DIR__.'/migrations/2019_01_12_150110_create_mail_emails_table.php');
-        $this->loadMigrationsFrom(__DIR__.'/migrations/2019_01_12_150130_create_mail_attachments_table.php');
+        
+        //$this->loadMigrationsFrom(__DIR__.'/migrations/2019_01_11_142447_create_mail_accounts_table.php');
+        //$this->loadMigrationsFrom(__DIR__.'/migrations/2019_01_12_134154_create_mail_options_table.php');
+        //$this->loadMigrationsFrom(__DIR__.'/migrations/2019_01_12_150110_create_mail_emails_table.php');
+        //$this->loadMigrationsFrom(__DIR__.'/migrations/2019_01_12_150130_create_mail_attachments_table.php');
 
         $this->publishes([ __DIR__.'/publish/css' => public_path('css'),], 'budisteikul');
         $this->publishes([ __DIR__.'/publish/js' => public_path('js'),], 'budisteikul');
